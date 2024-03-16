05a9160ed4d1ee5d8852b796921066a2bca196ea
yesh2103 <130658544+yesh2103@users.noreply.github.com>
2024-03-16 19:47:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function propose() {
+	document.getElementById("message").innerText = "Of course, I will!";
+	document.getElementById("proposal").style.backgroundColor = "#4CAF50";
+	document.getElementById("proposal").style.borderColor = "#3e8e41";
+	document.getElementById("proposal").style.boxShadow = "0 0 10px rgba(76, 175, 80, 0.5)";
+}
